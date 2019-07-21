46696600ac189f98a3bb0f1a36186b722f6387e1
digitaltwinnn <45341252+digitaltwinnn@users.noreply.github.com>
2019-07-21 10:09:59 +0200

    separate GreetingService
    Added separate service and switched to Dependency Injection. Removed target folders.

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -3,19 +3,32 @@ package org.acme.quickstart;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
+    @Test    
     public void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200)    
+             .body(is("hello\n"));
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(is("hello " + uuid + "\n"));
     }
 
 }
